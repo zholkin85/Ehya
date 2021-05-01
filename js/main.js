af508828fec68b2a4d9078d7882ecc125ffe93b9
zholkin85 <zholkin85@yandex.ru>
2021-05-01 11:57:17 +0300

    create: favicon

--- js/main.js
+++ js/main.js
@@ -201,4 +201,22 @@ $(document).ready(function() {
         }
         req.send(new FormData(event.target))
     }
+
+    // === кнопка наверх
+    var button = $('#button-up')
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            button.fadeIn()
+        } else {
+            button.fadeOut()
+        }
+    })
+    button.on('click', function() {
+        $('body, html').animate({
+                scrollTop: 0,
+            },
+            800,
+        )
+        return false
+    })
 })
